f04afacaf7145d906d1f091af3ccf737f3f286ef
ronnie7866 <mohdamir7505@gmail.com>
2024-06-03 12:39:50 +0500

    added buyer service

--- src/main/java/com/backend/ecommerce/repository/BuyerRepository.java
+++ src/main/java/com/backend/ecommerce/repository/BuyerRepository.java
@@ -1,13 +1,13 @@
-//package com.backend.ecommerce.repository;
-//
-//import com.backend.ecommerce.entity.Buyer;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//import static ch.qos.logback.core.joran.spi.ConsoleTarget.findByName;
-//
-//@Repository
-//public interface BuyerRepository extends JpaRepository<Buyer, Integer> {
-//}
+package com.backend.ecommerce.repository;
+
+import com.backend.ecommerce.entity.Buyer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static ch.qos.logback.core.joran.spi.ConsoleTarget.findByName;
+
+@Repository
+public interface BuyerRepository extends JpaRepository<Buyer, Integer> {
+}
